b90d4c93f1a473cf741d68b63c022dbc4ca81f3f
theobaidur <theobaidur@gmail.com>
2017-07-21 03:50:41 +0600

    adding component using angular cli and styling components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TutorialsComponent } from './tutorials/tutorials.component';
+import { InstructorsComponent } from './instructors/instructors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TutorialsComponent,
+    InstructorsComponent
   ],
   imports: [
     BrowserModule
